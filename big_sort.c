d19713c4779a2c8f8d159171618e1730645ca931
Wen-tzu LO <wlo@e1r11p22.42.fr>
2021-09-06 17:56:17 +0200

    try sort big

--- big_sort.c
+++ big_sort.c
@@ -1,4 +1,31 @@
 
-#include "push_swap.h"
-// #include <stdlib.h>
-// #include <stdio.h>
+#include "libft/libft.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int *sort_arry(int argc, char**argv)
+{
+	int *arr;
+	int i;
+
+	arr = (int*)malloc(argc * sizeof(int));
+	if (!arr)
+		return (0);
+	i = 0;
+	while(i < argc - 1)
+	{
+		arr[i] = ft_atoi(argv[i+1]);
+		i++;
+	}
+	
+	return arr;
+}
+
+int main (int argc, char** argv)
+{
+	int *arr;
+
+	arr = sort_arry(argc, argv);
+	return 0 ;
+
+}
--- struct_arr.c
+++ struct_arr.c
@@ -52,14 +52,11 @@ void	ft_lstadd_back_Doubly(t_listd **lst, t_listd *new)
 }
 void print_out(t_listd *arr_t)
 {
-<<<<<<< HEAD
 	if (arr_t == 0)
 	{
 		printf("arr: []\n");
 		return;
 	}
-=======
->>>>>>> ce0de38568ed6bfe0ace698ba88b126a19a113f0
 	printf("arr : [");
 	while (arr_t->next)
 	{	
@@ -106,11 +103,7 @@ void	ft_lstdel_Doubly(t_listd **lst, t_listd *del)
 	free(del);
 }
 
-<<<<<<< HEAD
 void swap_a(t_listd **lst)
-=======
-void operation_swap_a(t_listd **lst)
->>>>>>> ce0de38568ed6bfe0ace698ba88b126a19a113f0
 {
 	t_listd *temp;
 
@@ -145,10 +138,7 @@ void operation_swap_b(t_listd **lst)
 	free(temp);
 	write(1, "sb\n", 3);
 }
-<<<<<<< HEAD
 void push_a(t_listd **lst_a, t_listd **lst_b)
-=======
-void operation_push_a(t_listd **lst_a, t_listd **lst_b)
 {
 	t_listd *temp;
 
@@ -159,21 +149,6 @@ void operation_push_a(t_listd **lst_a, t_listd **lst_b)
 	temp->prev = (*lst_b)->prev;
 	ft_lstadd_front_Doubly(lst_a, temp);
 	ft_lstdel_Doubly(lst_b, *lst_b);
-	write(1, "pa\n", 3);
-}
-void operation_push_b(t_listd **lst_a, t_listd **lst_b)
->>>>>>> ce0de38568ed6bfe0ace698ba88b126a19a113f0
-{
-	t_listd *temp;
-
-	if (!(*lst_b))
-		return;
-	temp = ft_lstnew_Doubly((*lst_b)->content);
-	temp->next =(*lst_b)->next;
-	temp->prev = (*lst_b)->prev;
-	ft_lstadd_front_Doubly(lst_a, temp);
-	ft_lstdel_Doubly(lst_b, *lst_b);
-<<<<<<< HEAD
 	write(1, "pa\n", 3);
 }
 void push_b(t_listd **lst_a, t_listd **lst_b)
@@ -206,11 +181,6 @@ void rotate_a(t_listd **lst)
 	write(1, "ra\n", 3);
 }
 void rotate_b(t_listd **lst)
-=======
-	write(1, "pb\n", 3);
-}
-void operation_rotate_a(t_listd **lst)
->>>>>>> ce0de38568ed6bfe0ace698ba88b126a19a113f0
 {
 	t_listd *temp;
 	t_listd *key;
@@ -224,34 +194,14 @@ void operation_rotate_a(t_listd **lst)
 	key->prev = temp;
 	key->next = 0;
 	(*lst)->prev = 0;
-<<<<<<< HEAD
 	write(1, "rb\n", 3);
-=======
-	write(1, "ra\n", 3);
->>>>>>> ce0de38568ed6bfe0ace698ba88b126a19a113f0
 }
 void operation_rotate_b(t_listd **lst)
 {
 	t_listd *temp;
 	t_listd *key;
 
-<<<<<<< HEAD
 void r_rotate_a(t_listd **lst)
-=======
-	key = (*lst);
-	temp = (*lst);
-	(*lst) = (*lst)->next;
-	while(temp->next != 0)
-		temp = temp->next;
-	temp->next = key;
-	key->prev = temp;
-	key->next = 0;
-	(*lst)->prev = 0;
-	write(1, "rb\n", 3);
-}
-
-void operation_r_rotate_a(t_listd **lst)
->>>>>>> ce0de38568ed6bfe0ace698ba88b126a19a113f0
 {
 	t_listd *temp;
 	t_listd *key;
@@ -268,11 +218,7 @@ void operation_r_rotate_a(t_listd **lst)
 	write(1, "rra\n", 4);
 }
 
-<<<<<<< HEAD
 void r_rotate_b(t_listd **lst)
-=======
-void operation_r_rotate_b(t_listd **lst)
->>>>>>> ce0de38568ed6bfe0ace698ba88b126a19a113f0
 {
 	t_listd *temp;
 	t_listd *key;
@@ -302,7 +248,6 @@ void parse_lst(int argc, char **argv, t_listd **arr, t_listd **temp)
 	}
 }
 int confrim_sort(t_listd *arr)
-<<<<<<< HEAD
 {
 	int incre;
 	int decre;
@@ -442,121 +387,6 @@ int main(int argc, char**argv)
 	// t_listd *arr_1 = 0;
 	// t_listd *arr_2 = 0;
 	// t_listd *arr;
-=======
-{
-	int incre;
-	int decre;
-
-	incre = 0;
-	decre = 0;
-	while(arr->next)
-	{
-		if(arr->content > arr->next->content)
-			incre = 1;
-		if(arr->content < arr->next->content)
-			decre = 1;
-		arr = arr->next;
-	}
-	return (incre == decre? 0 : 1);
-}
-
-int the_max(t_listd *arr)
-{
-	int max;
-
-	max = arr->content;
-	while(arr->next)
-	{
-		if (arr->content > max)
-			max = arr->content;
-		arr = arr->next;
-	}
-	if (arr->content > max)
-			max = arr->content;
-	return (max);
-}
-
-int the_min(t_listd *arr)
-{
-	int min;
-
-	min = arr->content;
-	while(arr->next)
-	{
-		if (arr->content < min)
-			min = arr->content;
-		arr = arr->next;
-	}
-	if (arr->content < min)
-			min = arr->content;
-	return (min);
-}
-
-t_listd *small_sort_3(t_listd *arr , t_listd *current)
-{
-	if (current->content == the_max(arr))
-	{
-		current = current->next;
-		if (current->content == the_min(arr))
-			operation_rotate_a(&arr);
-		else
-		{
-			operation_swap_a(&arr);
-			operation_r_rotate_a(&arr);
-		}
-	}
-	else if (current->content == the_min(arr))
-	{
-		operation_swap_a(&arr);
-		operation_rotate_a(&arr);
-	}
-	else 
-	{
-		current = current->next;
-		if (current->content == the_min(arr))
-			operation_swap_a(&arr);
-		else
-			operation_r_rotate_a(&arr);
-	}
-	return arr;
-}
-
-t_listd *small_sort_5(t_listd *arr , t_listd *current)
-{
-	t_listd *arr_b;
-
-	arr_b = 0;
-	operation_push_b(&arr_b, &arr);
-	operation_push_b(&arr_b, &arr);
-	arr = small_sort_3(arr , arr);
-	print_out(arr);
-	current = arr;
-	while(current->next != 0)
-	{
-		//print_out(arr_b);
-		printf("arr_b->content :%d\n", arr_b->content);
-		printf("current->content :%d\n", current->content);
-		// if (arr_b->content < current->content)
-		// {	
-		// 	operation_push_b(&arr, &arr_b);
-		// 	printf("a < b\n");
-		// 	print_out(arr);
-		// 	arr_b = arr_b->next;
-		// }
-		current = current->next;
-		operation_rotate_a(&arr);
-		printf("current->content :%d\n", current->content);
-		printf("current->content :%d\n", current->next->content);
-		//print_out(arr);
-	}
-	return arr;
-}
-
-int main(int argc, char **argv)
-{
-	t_listd *arr_a;
-	t_listd *temp;
->>>>>>> ce0de38568ed6bfe0ace698ba88b126a19a113f0
 	//int i;
 
 	if (argc == 1)
@@ -580,17 +410,9 @@ int main(int argc, char **argv)
 	// 	ft_lstadd_back_Doubly(&arr_2 , arr);
 	// 	i++;
 	// }
-<<<<<<< HEAD
 	parse_lst(argc, argv,&arr_a ,&temp);
 	//print_out(arr_a);
 	arr_a = small_sort_5(arr_a, arr_a);
 	//print_out(arr_a);
-=======
-	
-	parse_lst(argc, argv,&arr_a ,&temp);
-	print_out(arr_a);
-	arr_a = small_sort_5(arr_a, arr_a);
-	print_out(arr_a);
->>>>>>> ce0de38568ed6bfe0ace698ba88b126a19a113f0
 	return 0;
 }
